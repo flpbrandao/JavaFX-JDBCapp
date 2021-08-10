7bd5ae37b67e5267795cc530bbafa50b259df57f
Felipe Oliveira <flpbrandao@hotmail.com>
2021-08-10 14:23:48 -0300

    Added initial DB configuration - apparently successful

--- src/gui/AppointmentsController.java
+++ src/gui/AppointmentsController.java
@@ -1,12 +1,14 @@
 package gui;
 
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import db.DB;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -35,6 +37,9 @@ public class AppointmentsController implements Initializable {
 
 	@FXML
 	private Button btRegister;
+	
+	@FXML
+	private Button btSearch;
 
 	@FXML
 	private void onBtRegisterAction() {
@@ -46,18 +51,29 @@ public class AppointmentsController implements Initializable {
 		System.out.println("Button search");
 	}
 
-	@FXML
-	private Button btSearch;
-
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
-		System.out.println("Init");
+		
+	}
+	private ObservableList<Appointment> obsList;
+	
+	private void popFields() { //Section made by my own
+		PopulateFields popTable = new PopulateFields();
+		List<Appointment> listAppointments = new ArrayList<>();
+		listAppointments = popTable.populateTableViewList();
+		obsList = FXCollections.observableArrayList(listAppointments);
+		tableViewAppointment.setItems(obsList);
+	}
+	private void initializeDB() {
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
 	}
 
 	private void initializeNodes() {
 		
-		this.popFields(); //Added function to populate tableView on form initialization - worked
+		initializeDB();
+		popFields(); //Added function to populate tableView on form initialization - worked
 
 		tableColumnDescription.setCellValueFactory(new PropertyValueFactory<>("Description"));
 		tableColumnDate.setCellValueFactory(new PropertyValueFactory<>("Date"));
@@ -68,14 +84,7 @@ public class AppointmentsController implements Initializable {
 
 	}
 
-	private void popFields() { //Section made by my own
-		PopulateFields popTable = new PopulateFields();
-		List<Appointment> listAppointments = new ArrayList<>();
-		listAppointments = popTable.populateTableViewList();
-		obsList = FXCollections.observableArrayList(listAppointments);
-		tableViewAppointment.setItems(obsList);
-	}
 
-	private ObservableList<Appointment> obsList;
+	
 
 }
